d5052dd7c9ea6d4a390551868cad68f982f90533
Hryhorii Khyzhniakov <soults15@gmail.com>
2021-02-22 18:18:02 +0200

    Basic containers showcase

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Container, makeStyles, colors } from '@material-ui/core';
 
-import { GradientButton } from '../styled';
-import { Clock } from '.';
+const useStyles = makeStyles({
+  red: { backgroundColor: colors.red[100] },
+  blue: { backgroundColor: colors.blue[100] },
+  green: { backgroundColor: colors.green[100] },
+  orange: { backgroundColor: colors.orange[100] },
+});
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <Typography variant="h3">
-        React App with Material UI
-      </Typography>
-
-      <GradientButton>With Styled Components API</GradientButton>
-
-      <Clock />
+      <Container className={classes.red}>
+        <Typography>Fluid container</Typography>
+      </Container>
+      <Container className={classes.blue} fixed>
+        <Typography>Fixed container</Typography>
+      </Container>
+      <Container className={classes.green} maxWidth="xs">
+        <Typography>Container with maxWidth="xs"</Typography>
+      </Container>
+      <Container className={classes.orange} disableGutters>
+        <Typography>No gutters</Typography>
+      </Container>
     </>
   );
 };
